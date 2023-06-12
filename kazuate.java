7265cce77e3247ad41fbd5045e064834c1c8720f
YuKata35 <toyu3535@gmail.com>
2023-06-12 12:12:04 +0900

    修正2

--- kazuate.java
+++ kazuate.java
@@ -1,6 +1,7 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class  kazuate{
+public class kazuate {
     public static void main(String[] args) {
         // 設定された数
         int targetNumber = 42;
@@ -12,27 +13,34 @@ public class  kazuate{
         while (attempts < 5) {
             Scanner scanner = new Scanner(System.in);
             System.out.print("2桁の正の整数を入力してください: ");
-            int guess = scanner.nextInt();
-            attempts++;
 
-            // 入力された数と設定された数を比較
-            if (guess == targetNumber) {
-                System.out.println("当たり！");
-                break;
-            } else {
-                System.out.println("外れました。");
+            try {
+                int guess = scanner.nextInt();
+                attempts++;
 
-                // 数の大小をユーザにフィードバックする
-                if (guess > targetNumber) {
-                    System.out.println("設定された数よりも小さい数です。");
+                // 入力された数と設定された数を比較
+                if (guess == targetNumber) {
+                    System.out.println("当たり！");
+                    scanner.close();
+                    break;
                 } else {
-                    System.out.println("設定された数よりも大きい数です。");
-                }
+                    System.out.println("外れました。");
 
-                // 20以上差がある場合のメッセージを表示
-                if (Math.abs(guess - targetNumber) >= 20) {
-                    System.out.println("20以上の差があります。");
+                    // 数の大小をユーザにフィードバックする
+                    if (guess > targetNumber) {
+                        System.out.println("設定された数よりも小さい数です。");
+                    } else {
+                        System.out.println("設定された数よりも大きい数です。");
+                    }
+
+                    // 20以上差がある場合のメッセージを表示
+                    if (Math.abs(guess - targetNumber) >= 20) {
+                        System.out.println("20以上の差があります。");
+                    }
                 }
+            } catch (InputMismatchException e) {
+                System.out.println("数値を入力してください。");
+                scanner.next(); // 入力バッファをクリアする
             }
         }
 
